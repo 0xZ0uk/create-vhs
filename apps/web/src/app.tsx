66682f52e4a868dc00f78356e1dc0f43cd55bc74
Pedro Santana <pedrsntana@proton.me>
2025-06-06 18:58:09 +0100

    Update example

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,19 +1,22 @@
+import { ColorModeProvider, ColorModeScript } from "@kobalte/core";
+import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Nav from "./components/Nav";
 
 import "@vhs/ui/styles/globals.css";
-import { MetaProvider, Title } from "@solidjs/meta";
 
 export default function App() {
 	return (
 		<Router
 			root={(props) => (
-				<>
-					<Nav />
-					<Suspense>{props.children}</Suspense>
-				</>
+				<MetaProvider>
+					<Title>VHS</Title>
+					<Suspense>
+						<ColorModeScript />
+						<ColorModeProvider>{props.children}</ColorModeProvider>
+					</Suspense>
+				</MetaProvider>
 			)}
 		>
 			<FileRoutes />
--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -6,31 +6,13 @@ export default function Home() {
 	return (
 		<MetaProvider>
 			<Title>VHS</Title>
-			<main class="mx-auto p-4 text-center text-gray-700">
-				<h1 class="max-6-xs my-16 font-thin text-6xl text-sky-700 uppercase">
+			<main class="mx-auto p-4 text-center text-foreground">
+				<h1 class="max-6-xs my-16 font-bold text-6xl text-primary uppercase">
 					VHS
 				</h1>
 				<p>Vite + Hono + Solid</p>
+
 				<Button>Hello</Button>
-				<p class="mt-8">
-					Visit{" "}
-					<a
-						href="https://solidjs.com"
-						target="_blank"
-						class="text-sky-600 hover:underline"
-						rel="noreferrer"
-					>
-						solidjs.com
-					</a>{" "}
-					to learn how to build Solid apps.
-				</p>
-				<p class="my-4">
-					<span>Home</span>
-					{" - "}
-					<A href="/about" class="text-sky-600 hover:underline">
-						About Page
-					</A>{" "}
-				</p>
 			</main>
 		</MetaProvider>
 	);
