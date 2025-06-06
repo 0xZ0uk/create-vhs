b2ecc4d35361abd818a8cb4ed1746d87f6d95876
Pedro Santana <pedrsntana@proton.me>
2025-06-06 17:50:08 +0100

    Improve @vhs/ui package

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -1,20 +1,21 @@
+import Nav from "@/components/Nav";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
-import "./app.css";
+
+import "@vhs/ui/styles/globals.css";
 
 export default function App() {
-  return (
-    <Router
-      root={props => (
-        <>
-          <Nav />
-          <Suspense>{props.children}</Suspense>
-        </>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+	return (
+		<Router
+			root={(props) => (
+				<>
+					<Nav />
+					<Suspense>{props.children}</Suspense>
+				</>
+			)}
+		>
+			<FileRoutes />
+		</Router>
+	);
 }
